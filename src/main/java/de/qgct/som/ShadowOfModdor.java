9d1435e5dbe18facd51a49f6976b9acea66041cf
Marcel <kingofdog@web.de>
2017-11-19 15:07:24 +0100

    Add basic structure

---
+++ src/main/java/de/qgct/som/ShadowOfModdor.java
@@ -0,0 +1,15 @@
+package de.qgct.som;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "shadowofmoddor", useMetadata = true)
+public class ShadowOfModdor
+{
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
