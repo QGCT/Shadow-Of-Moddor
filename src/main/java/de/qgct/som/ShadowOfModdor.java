60b45a0763c3456b313661d90a5caeaad0676695
Marcel <kingofdog@web.de>
2017-11-19 22:33:02 +0100

    Add PreInit to Main Class Change .gitignore

--- src/main/java/de/qgct/som/ShadowOfModdor.java
+++ src/main/java/de/qgct/som/ShadowOfModdor.java
@@ -3,22 +3,21 @@ package de.qgct.som;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import de.qgct.som.common.item.ItemReinforcedDiamond;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import de.qgct.som.common.manager.BlockManager;
+import de.qgct.som.common.manager.OreManager;
 
 @Mod(modid = "shadowofmoddor", useMetadata = true)
 public class ShadowOfModdor {
 
-
-    ItemReinforcedDiamond itemReinforcedDiamond;
-
-
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        BlockManager.createBlocks();
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
-        itemReinforcedDiamond = new ItemReinforcedDiamond();
-
-
-
+        GameRegistry.registerWorldGenerator(new OreManager(), 100);
     }
 }
--- src/main/java/de/qgct/som/common/item/ItemReinforcedDiamond.java
+++ src/main/java/de/qgct/som/common/item/ItemReinforcedDiamond.java
@@ -3,19 +3,14 @@ package de.qgct.som.common.item;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-public class ItemReinforcedDiamond extends Item  {
+public class ItemReinforcedDiamond extends Item {
 
+    public ItemReinforcedDiamond() {
 
-
-        public ItemReinforcedDiamond(){
-
-            setCreativeTab(CreativeTabs.tabMaterials);
-            setMaxStackSize(64);
-            setUnlocalizedName("ItemReinforcedDiamond");
-            setTextureName("");
-
-
-
-        }
-
+        setCreativeTab(CreativeTabs.tabMaterials);
+        setMaxStackSize(64);
+        setUnlocalizedName("ItemReinforcedDiamond");
+        setTextureName("");
     }
+
+}
