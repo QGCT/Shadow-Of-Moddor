4a588a1c0d5a2fe47e2b417af14427a0cb6844b6
Marcel <kingofdog@web.de>
2017-11-19 21:40:44 +0100

    Implement Ore Manager to generate ores

---
+++ src/main/java/de/qgct/som/common/block/ore/OreBlockPlatinum.java
@@ -0,0 +1,4 @@
+package de.qgct.som.common.block.ore;
+
+public class OreBlockPlatinum {
+}
---
+++ src/main/java/de/qgct/som/common/manager/OreManager.java
@@ -0,0 +1,54 @@
+package de.qgct.som.common.manager;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
+import java.util.Random;
+
+public class OreManager implements IWorldGenerator {
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        switch (world.provider.dimensionId) {
+            case -1:
+                generateNether(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 0:
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 1:
+                generateEnd(world, random, chunkX * 16, chunkZ * 16);
+                break;
+        }
+    }
+
+    // Generate the Surface World
+    private void generateSurface(World world, Random random, int x, int z) {
+
+    }
+
+    // Generate the End Dimension
+    private void generateEnd(World world, Random random, int x, int z) {
+
+    }
+
+    // Generate the Nether Dimension
+    private void generateNether(World world, Random random, int x, int z) {
+
+    }
+
+    // Function to add a new ore
+    public void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxX, int maxZ, int maxVeinSize, int chancesToSpawn, int minY, int maxY) {
+        int maxPosY = minY + (maxY - 1);
+        int deltaMinMaxY = maxY - minY;
+        for (int x = 0; x < chancesToSpawn; x++) {
+            int posX = blockXPos + random.nextInt(maxX);
+            int posY = minY + random.nextInt(deltaMinMaxY);
+            int posZ = blockZPos + random.nextInt(maxZ);
+            (new WorldGenMinable(block, maxVeinSize)).generate(world, random, posX, posY, posZ);
+        }
+    }
+}
