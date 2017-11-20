8dd3b083de2753721bd9c3c53fd6238e273a7262
Marcel <kingofdog@web.de>
2017-11-20 19:36:46 +0100

    Call of ItemManager

--- src/main/java/de/qgct/som/ShadowOfModdor.java
+++ src/main/java/de/qgct/som/ShadowOfModdor.java
@@ -6,14 +6,19 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import de.qgct.som.common.manager.BlockManager;
+import de.qgct.som.common.manager.ItemManager;
 import de.qgct.som.common.manager.OreManager;
 
-@Mod(modid = "shadowofmoddor", useMetadata = true)
+@Mod(modid = ShadowOfModdor.modID, useMetadata = true)
 public class ShadowOfModdor {
+
+    public static final String modID = "shadowofmoddor";
+    public static final String modName = "Shadow Of Moddor";
 
     @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
         BlockManager.createBlocks();
+        ItemManager.createItems();
     }
 
     @EventHandler
